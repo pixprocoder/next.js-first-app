480b99d9c4b1096759c5cc32b1acf27ee80d6b46
skpixels <makkobir79@gmail.com>
2023-07-25 17:55:45 +0300

    ant design installed

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,7 +1,13 @@
+import { Button } from "antd";
 import React from "react";
 
 function ProductsHomePage() {
-  return <div>Products home page</div>;
+  return (
+    <div>
+      <h1>Products home page</h1>
+      <Button type="primary">Hello 👋🏼</Button>
+    </div>
+  );
 }
 
 export default ProductsHomePage;
